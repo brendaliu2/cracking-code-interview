1735d04afcece2a1e37b5e9377441151ab45b8aa
Brenda Liu <brenda.liu24@gmail.com>
2022-11-14 23:35:57 -0600

    complete 3.3

---
+++ stackOfPlates.js
@@ -0,0 +1,27 @@
+class StackOfPlates {
+  constructor() {
+    this.stackSet = [[]];
+    this.maxHeight = 5;
+  }
+
+  pushVal(val) {
+    const lastStack = this.stackSet.at(-1);
+    if (lastStack.length < this.maxHeight) {
+      this.stackSet.at(-1).push(val);
+    } else {
+      this.stackSet.push([val]);
+    }
+  }
+
+  popVal() {
+    if (!this.stackSet.length) return 'Empty Stack';
+    const popped = this.stackSet.at(-1).pop();
+    const lastStack = this.stackSet.at(-1);
+    if (!lastStack.length) {
+      this.stackSet.pop();
+      return popped;
+    } else {
+      return popped;
+    }
+  }
+}
